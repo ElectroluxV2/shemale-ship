715e6353c7579e9b6f8aee6e3051740ae06f3b01
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-25 01:32:35 +0200

    start

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.onload = () => {
+    window.requestAnimationFrame(a);
+}
+
+const a = () => {
+    const canvas = document.getElementsByTagName('canvas')[0];
+
+    console.log(canvas);
+}
