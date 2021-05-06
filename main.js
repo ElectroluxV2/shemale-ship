02881827376a9ad5e5a0596a8db3578110d6840c
ElectroluxV2 <electroluxv2@gmail.com>
2021-05-06 22:27:03 +0200

    Dupa 3

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let currentFrameRequestId;
 const entities = [];
+const keyboardSates = new Map();
 
 const mainLoop = () => {
 
-    // context.reset();
+    if (keyboardSates["ArrowRight"]) {
+        entities[0].pos.x++;
+    }
+
+    if (keyboardSates["ArrowLeft"]) {
+        entities[0].pos.x--;
+    }
+
+    context.reset();
 
     for (const entity of entities) {
         entity.draw(context);
@@ -26,6 +35,14 @@ const initialize = () => {
         canvas.height = canvas.parentElement.offsetHeight * window.devicePixelRatio;
         canvas.width = canvas.parentElement.offsetWidth * window.devicePixelRatio;
         context.scale(window.devicePixelRatio, window.devicePixelRatio);
+    }
+
+    window.onkeydown = event => {
+        keyboardSates[event.key] = true;
+    }
+
+    window.onkeyup = event => {
+        keyboardSates[event.key] = false;
     }
 
     const ship = new Ship();
--- ship.js
+++ ship.js
@@ -1,29 +1,30 @@
 import { Pos } from "./pos.js"
+
 export class Ship {
+
     constructor() {
         console.log('ship');
-        this.pos.setPos(0, 0)
-        this.pos.setAngle(0)
-    }
-    pos = new Pos()
+        this.pos.setPos(0, 0);
+        this.pos.setAngle(0);
+    };
+
+    pos = new Pos();
 
     draw (ctx) {
         // center
-
-        ctx.fillStyle = "#FF0000"
-        ctx.fillRect(this.pos.x-5, this.pos.y-5, 10, 10)
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(this.pos.x-2, this.pos.y-2, 4, 4);
 
         // body
-
-        ctx.translate(this.pos.x, this.pos.y)
-        ctx.rotate(this.pos.angle * Math.PI / 180)
-        ctx.translate(-this.pos.x, -this.pos.y)
-        ctx.strokeStyle = "#FFF"
+        ctx.translate(this.pos.x, this.pos.y);
+        ctx.rotate(this.pos.angle * Math.PI / 180);
+        ctx.translate(-this.pos.x, -this.pos.y);
+        ctx.strokeStyle = "#FFF";
         ctx.beginPath();
-        ctx.moveTo(this.pos.x-36, this.pos.y+25)
-        ctx.lineTo(this.pos.x+36, this.pos.y+25)
-        ctx.lineTo(this.pos.x, this.pos.y-65)
-        ctx.closePath()
-        ctx.stroke()
-    }
+        ctx.moveTo(this.pos.x-36, this.pos.y+25);
+        ctx.lineTo(this.pos.x+36, this.pos.y+25);
+        ctx.lineTo(this.pos.x, this.pos.y-65);
+        ctx.closePath();
+        ctx.stroke();
+    };
 }
