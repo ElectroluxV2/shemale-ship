b928edd6f93c864ff5f40959d023d9e4d9333c9a
Bartłomiej Wujec <bartek99q@gmail.com>
2021-05-06 20:46:38 +0200

    Dodano rysowanie statku

--- ship.js
+++ ship.js
@@ -1,5 +1,32 @@
+import { Pos } from "./pos.js"
 export class Ship {
-    constructor() {
+    constructor(contex) {
         console.log('ship');
+        this.ctx = contex;
+        this.pos.setPos(0, 0)
+        this.pos.setAngle(0)
+    }
+    ctx = null;
+    pos = new Pos()
+    draw () {
+        // center
+
+        this.ctx.fillStyle = "#FF0000"
+        this.ctx.fillRect(this.pos.x-5, this.pos.y-5, 10, 10)
+
+        // body
+
+        this.ctx.translate(this.pos.x, this.pos.y)
+        this.ctx.rotate(this.angle * Math.PI / 180)
+        this.ctx.translate(-this.pos.x, -this.pos.y)
+        this.ctx.strokeStyle = "#FFF"
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.pos.x-36, this.pos.y+25)
+        this.ctx.lineTo(this.pos.x+36, this.pos.y+25)
+        this.ctx.lineTo(this.pos.x, this.pos.y-65)
+        this.ctx.closePath()
+        this.ctx.stroke()
+
+
     }
 }
