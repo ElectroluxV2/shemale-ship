32dc6ee25bb058c0c230fb77b9c2f107b76d73f3
Bartłomiej Wujec <bartek99q@gmail.com>
2021-05-08 01:11:43 +0200

    Żyrafy wchodzą na szafy

---
+++ entity.js
@@ -0,0 +1,20 @@
+import { Pos } from "./pos.js"
+import { Vector } from "./vector.js";
+import { Point } from "./point.js";
+
+export class Entity {
+    #position;
+    #previousPosition;
+
+    constructor(position = new Pos()) {
+        this.#position = position;
+    }
+
+    angledVector() {
+        return Vector.j.multiply(Math.cos(this.#position.radians)).add(Vector.i.multiply(Math.sin(this.#position.radians)));
+    }
+
+    get pos() {
+        return this.#position;
+    }
+}
